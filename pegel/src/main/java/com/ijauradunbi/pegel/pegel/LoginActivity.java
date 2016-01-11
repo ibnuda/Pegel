078efa9389d7ce3c2cfaa5970dd113155a31eb0d
ibnuda <ibnu.d.a@student.uns.ac.id>
2016-01-11 10:41:08 +0700

    init commit

---
+++ pegel/src/main/java/com/ijauradunbi/pegel/pegel/LoginActivity.java
@@ -0,0 +1,102 @@
+package com.ijauradunbi.pegel.pegel;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * Created by IT Support on 2016-01-11.
+ */
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
+    @Bind(R.id.input_email) EditText emailText;
+    @Bind(R.id.input_password) EditText passwordText;
+    @Bind(R.id.button_login) Button loginButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+
+    public void login() {
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+        loginButton.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Logging in...");
+        progressDialog.show();
+
+        String email = emailText.getText().toString();
+        String password = emailText.getText().toString();
+
+        if (email.equals("ibnu@ganteng.com") && password.equals("asdf")) {
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        @Override
+                        public void run() {
+                            onLoginSuccess();
+                            progressDialog.dismiss();
+                        }
+                    }, 3000
+            );
+
+        }
+    }
+
+    public void onLoginSuccess() {
+        loginButton.setEnabled(true);
+        finish();
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login gagal.", Toast.LENGTH_LONG).show();
+        loginButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailText.setText("email yang sesuai.");
+            valid = false;
+        } else {
+            emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4) {
+            passwordText.setError("password setidaknya 4 karakter.");
+        } else {
+            passwordText.setError(null);
+        }
+
+        return valid;
+    }
+}
